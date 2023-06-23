a806ba8250138e37ddc957aba290cac771d86022
love-sonkar <loveboss806@gmail.com>
2023-06-23 09:48:28 +0530

    queue practice code added

---
+++ queue.js
@@ -0,0 +1,30 @@
+class Queue {
+  constructor(maxvalue = 0) {
+    this.Queue = [];
+    this.maxvalue = maxvalue - 1;
+  }
+
+  enqueue(newVal) {
+    if (this.Queue.length > this.maxvalue) return console.log("queue is full");
+    this.Queue[this.Queue.length] = newVal;
+  }
+
+  dequeue() {
+    if (this.Queue.length === 0) return console.log("queue is empty");
+    for (let i = 0; i < this.Queue.length; i++) {
+      this.Queue[i] = this.Queue[i + 1];
+    }
+    this.Queue.length = this.Queue.length - 1;
+  }
+
+  display() {
+    console.log(this.Queue);
+  }
+}
+
+let queue = new Queue(2);
+
+queue.enqueue(54);
+queue.enqueue(8);
+
+queue.display();
