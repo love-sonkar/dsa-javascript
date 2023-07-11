c7da1f217504f9170908773f122b7ffa6ba7dd97
love-sonkar <loveboss806@gmail.com>
2023-07-11 17:19:33 +0530

    linked list append node learning using function's

--- linkedlist.js
+++ linkedlist.js
@@ -22,8 +22,38 @@ class List {
   }
 }
 
-let newlist = new List(4);
-newlist.appendNode(5);
-newlist.appendNode(6);
+// let newlist = new List(4);
+// newlist.appendNode(5);
+// newlist.appendNode(6);
 
-console.log(newlist);
+// console.log(newlist);
+
+function LinkdeList(x) {
+  this.head = {
+    value: x,
+    next: null,
+  };
+
+  this.tail = head;
+  this.size = 0;
+
+  let append = function (newval) {
+    let newNode = {
+      value: newval,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.size++;
+    return newNode;
+  };
+
+  return { head, tail, append };
+}
+
+let listfunc = LinkdeList(5);
+listfunc.append(6);
+listfunc.append(7);
+listfunc.append(8);
+
+console.log(listfunc);
