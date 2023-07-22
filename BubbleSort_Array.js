b06f102ffd4ef5bfc949c89613a03a42bf7492aa
love-sonkar <loveboss806@gmail.com>
2023-07-22 13:17:35 +0530

    added new code in bubble sort file

--- BubbleSort_Array.js
+++ BubbleSort_Array.js
@@ -24,3 +24,18 @@ console.log(BubbleSort(array));
 // then rest we get the sorted array in return
 
 // -----Thanks----
+
+function NewSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(NewSort(array));
